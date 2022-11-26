e188b37c1867a7d167881b19e38b6490b68eb07f
Víctor Jiménez <43785784+victor7w7r@users.noreply.github.com>
2022-11-26 17:19:53 -0500

    UpdateSDKTo2.18.5

--- lib/models/dummy.dart
+++ lib/models/dummy.dart
@@ -1,21 +1,14 @@
-import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart' show JsonSerializable;
 
 part '../generated/models/dummy.g.dart';
 
 @JsonSerializable()
-class Dummy extends Equatable {
+class Dummy {
 
   const Dummy(this.id, this.name);
 
   final String id;
   final String name;
-
-  @override
-  List<Object> get props => [id, name];
-
-  @override
-  bool get stringify => true;
 
   factory Dummy.fromJson(Map<String, dynamic> json) => _$DummyFromJson(json);
   Map<String, dynamic> toJson() => _$DummyToJson(this);
