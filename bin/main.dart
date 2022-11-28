9b49dfa341d3c803b00184be55ff97e22d072cf8
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-11-27 21:01:13 -0500

    ImproveCascadeOperator

--- bin/main.dart
+++ bin/main.dart
@@ -5,24 +5,26 @@ import 'package:dart_server/config/env.dart';
 
 Future<Handler> initialize() async {
 
-  final app = Router().plus;
-
-  app.use(corsHeaders(headers: {
+  final app = Router().plus
+    ..use(corsHeaders(headers: {
     ACCESS_CONTROL_ALLOW_ORIGIN: '*',
     ACCESS_CONTROL_ALLOW_CREDENTIALS: 'true',
     ACCESS_CONTROL_ALLOW_METHODS: 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
     ACCESS_CONTROL_ALLOW_HEADERS: 'Origin,Content-Type,Authorization,locale,x-api-key',
     ACCESS_CONTROL_MAX_AGE: '86400',
     'Content-Type': 'application/json;charset=utf-8'
-  }));
-
-  app.use(setContentType('application/json'));
+  }))
+    ..use(setContentType('application/json'));
 
   return app;
 }
-void main(List<String> args) => shelfRun(
-  initialize,
-  defaultBindAddress: '0.0.0.0',
-  defaultEnableHotReload: Env.app == "dev",
-  onStarted: (_, port) => print("Listening in port $port")
-);
+Future<void> main(List<String> args) async {
+
+  shelfRun(
+    initialize,
+    defaultBindAddress: '0.0.0.0',
+    defaultEnableHotReload: Env.app == "dev",
+    onStarted: (_, port) => print("Listening in port $port")
+  );
+
+}
